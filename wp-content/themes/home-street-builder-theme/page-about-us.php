8fe6d246bf4df57c40376ed05d693197c67ce68e
riff3r <arif4alll@gmail.com>
2024-03-18 01:51:10 +0600

    about page initially done

--- wp-content/themes/home-street-builder-theme/page-about-us.php
+++ wp-content/themes/home-street-builder-theme/page-about-us.php
@@ -2,17 +2,248 @@
 
 get_header();
 
-while (have_posts()){
+while (have_posts()) {
     the_post(); ?>
-        <div class="page-cover-photo" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);">
-            <div class="overlay_shadow"></div>
-            <h1 class="page-title ">
-                <?php the_title() ?>
-            </h1>
+    <div class="page-cover-photo" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);">
+        <div class="overlay_shadow"></div>
+        <h1 class="page-title ">
+            <?php the_title() ?>
+        </h1>
+    </div>
+
+
+    <div class="container mb-5">
+        <div class="about-us">
+            <div class="h_title mb-md-5 mb-sm-4 my-5 text-center">
+                <div class="_title">Introduction</div>
+                <div class="_title_line"></div>
+            </div>
+
+            <div class="about-description">
+                <p>
+                    Since its inception in 2010, Unimass Holdings Ltd. creates greater value of living for its Clients,
+                    Landowners and Stakeholders. Unimass Holdings Ltd. has made it a success story by establishing long
+                    term
+                    mutually beneficial relationship with its valued Clients, Prospects, Landowners, Patrons and
+                    Business
+                    Associates through quality, commitment and professionalism. With a team of experienced Management
+                    Members,
+                    Engineers and Consultants, Unimass Holdings Ltd. always gives top priority to the structural aspect
+                    of
+                    building construction, which ought to be the primary concern because a fanciful façade can never
+                    make up for
+                    a defective structure. Blazing the trail in modern urban housing trends since its advent onto the
+                    real
+                    estate arena, Unimass Holdings Ltd. has projects spread out all over Dhaka and Vicinity including
+                    renowned
+                    residential zones in Dhanmondi, Central Road, Green Road, Indira Road, Kalabagan, Eskaton Garden,
+                    New
+                    Eskaton, Mirpur DOHS, Mirpur, Pallabi, Mohammadpur, Kamalapur-Kabi Jashimuddin Road, West Dhanmondi,
+                    Rayer
+                    Bazar, Kallyanpur, Uttara, Bashundhara R/A, Aftabnagar, Bashabo, Lake-drive Road - Gulshan, Badda
+                    etc.
+
+                </p>
+
+                <p>
+                    At Unimass Holdings Ltd. the best interests of our patrons will always come first. Customer
+                    satisfaction is our focal point, because we understand that earning the satisfaction of our
+                    customer’s happiness beyond their expectations; because a satisfied customer becomes our ‘Advocate
+                    or Ambassador’. With a view to achieve this goal, we are dedicated to the development of long-term
+                    client relationships and gaining trust through responsiveness, flexibility and open communication by
+                    every member of our team. We are dedicated to continue the same pace and provide even better service
+                    and product and the achievement has imbued us with encourage and enthusiasm to do even better in the
+                    years to come
+                </p>
+
+            </div>
+
+            <div class="row">
+
+                <div class="col-lg-6 mission_section px_12">
+                    <div class="h_title mb-4 fs-3">
+                        <div class="_title">Our Mission</div>
+                        <div class="under_line_2"></div>
+                    </div>
+
+
+                    <div class="">
+
+                        <p>Unimass Holdings Limited is committed to providing the most professional, informative, loyal,
+                            and dedicated service in the industry by creating a successful partnership with customers
+                            throughout the process. The best interests of our patrons will always come first. We are
+                            dedicated to the development of long-term client relationships and gaining trust through
+                            responsiveness, flexibility, and open communication by every member of our team.<br></p>
+                        <p></p>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-6 vision_section px_12" id="abt_numeric_anim_area">
+                    <div class="h_title mb-4 fs-3">
+                        <div class="_title">Our Vision</div>
+                        <div class="under_line_2"></div>
+                    </div>
+
+                    <div class="">
+                        <p class="m_v_desc vision_desc_text_area">
+                            Unimass Holdings Limited will expand its efforts to grow profitably by meeting customer
+                            needs and satisfaction by establishing direct relationships with our customers. Considering
+                            good ecological demand we will accomplish this by responding to changing market conditions;&nbsp;
+                            building a performance-driven culture that continues to provide our customers with the
+                            highest quality residences and commercial complexes; improving job site productivity through
+                            effective job controls; maintaining a secure workplace and utilizing the latest technology
+                            and by creating an environment that encourages growth and development of our people.<br></p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="glance_section">
+        <div class="container">
+            <div class="h_title pt-3 mb-5 text-center">
+                <div class="_title">At A Glance</div>
+                <div class="_title_line"></div>
+            </div>
+
+            <div class="glance_content">
+                <div class="container">
+                    <div class="row justify-content-md-center mb-4">
+                        <div class="col col-lg-4">
+                            <h5 class="fw-bold">Name of The company</h5>
+                        </div>
+                        <div class="col col-lg-4">
+                            <p class="glance_desc">Unimass Holdings Limited</p>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-md-center mb-4">
+                        <div class="col col-lg-4">
+                            <h5 class="fw-bold">Corporate Office</h5>
+                        </div>
+                        <div class="col col-lg-4">
+                            <p class="glance_desc">House 04, Road 28(Old) 15 (New), Dhanmondi, Dhaka 1209</p>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-md-center mb-4">
+                        <div class="col col-lg-4">
+                            <h5 class="fw-bold">Registered Office</h5>
+                        </div>
+                        <div class="col col-lg-4">
+                            <p class="glance_desc">18, Kazi Nazrul Islam Avenue, Shahbagh, Dhaka 1000</p>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-md-center mb-4">
+                        <div class="col col-lg-4">
+                            <h5 class="fw-bold">Managing Director</h5>
+                        </div>
+                        <div class="col col-lg-4">
+                            <p class="glance_desc">Sayyedul Absar</p>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-md-center mb-4">
+                        <div class="col col-lg-4">
+                            <h5 class="fw-bold">Legal Status</h5>
+                        </div>
+                        <div class="col col-lg-4">
+                            <p class="glance_desc">Private Limited Company</p>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-md-center mb-4">
+                        <div class="col col-lg-4">
+                            <h5 class="fw-bold">Date Of Incorporation</h5>
+                        </div>
+                        <div class="col col-lg-4">
+                            <p class="glance_desc">01 January, 2010</p>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-md-center mb-4">
+                        <div class="col col-lg-4">
+                            <h5 class="fw-bold">Business Type</h5>
+                        </div>
+                        <div class="col col-lg-4">
+                            <p class="glance_desc">Real Estate</p>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-md-center mb-4">
+                        <div class="col col-lg-4">
+                            <h5 class="fw-bold">Products</h5>
+                        </div>
+                        <div class="col col-lg-4">
+                            <p class="glance_desc">Residential Apartment</p>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-md-center mb-4">
+                        <div class="col col-lg-4">
+                            <h5 class="fw-bold">Rehab Membership No.</h5>
+                        </div>
+                        <div class="col col-lg-4">
+                            <p class="glance_desc">1242/2012</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="management_section mt-5">
+        <div class="h_title pt-3 mb-5 text-center">
+            <div class="_title">Our Management Team
+            </div>
+            <div class="_title_line"></div>
         </div>
 
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="image_container_md">
+                        <img src="https://unimassbd.com/uploads/images/about/1646215927_3782_md.png" alt=""
+                             class="z-2 absolute md_image_style">
+                        <div class="fake_frame fake_frame_left"></div>
+                    </div>
+                </div>
 
-    content
+                <div class="col-lg-6 md_text_area my-auto">
+                    <div class="mb-5">
+                        <h2 class="fw-bold">Sayyedul Absar</h2>
+                        <p class="designation_section mb-2">MANAGING DIRECTOR, UNIMASS HOLDINGS LIMITED</p>
+                        <div class="under_line_2"></div>
+                    </div>
+
+                    <div class="management_desc">As Managing Director, he sets and evolves the strategic
+                        direction for the Company and its portfolio of
+                        offerings, while nurturing a strong leadership team to drive its execution. He has an experience
+                        of
+                        approximately over 2 decades in the real estate sector with a strong track record of executing
+                        business
+                        turnarounds and managing successful acquisitions. Earlier, he was one of the Promoters of Asset
+                        Developments &amp; Holdings Limited, he led this Company to become one of the leading real
+                        estate developers
+                        in Bangladesh by his self-motivated professional skills with outstanding leadership,
+                        problem-solving talents,
+                        exceptional organizational skills, implementing innovative management techniques and system
+                        organizer,
+                        establishing departmental policies and procedures, training and monitoring the sales and
+                        marketing force.
+                        He also held several leadership positions in Advanced Development &amp; Technologies Limited.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
     <?php
 //    the_content();
 
